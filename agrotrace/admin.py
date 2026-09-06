bb9e1df092dc0a5567dc7e1ae15a33562bdc93a4
mtl-124 <matteolajara@gmail.com>
2026-09-05 22:16:52 -0500

    Implementación CRUD completo Fundo y Lote - AGROTRACE

--- agrotrace/admin.py
+++ agrotrace/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import CentroAcopio, VariedadCultivo, CertificacionAgricola, FundoProductor, LoteRecepcionado
 
-# Register your models here.
+admin.site.register(CentroAcopio)
+admin.site.register(VariedadCultivo)
+admin.site.register(CertificacionAgricola)
+admin.site.register(FundoProductor)
+admin.site.register(LoteRecepcionado)
